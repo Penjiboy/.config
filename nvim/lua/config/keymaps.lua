855309e62e8d2de97d2ff1a082f0d673e4037f5e
Penjani Chavula <chavulapenjani@gmail.com>
2025-04-30 16:52:33 -0700

    add keymaps to navigate up/down the file tree

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -55,3 +55,58 @@ vim.keymap.set("n", "<leader>gl", function()
             .. vim.fn.expand("%:p")
     )
 end, {})
+
+-- adapted from https://github.com/kaiguogit/my-configs/blob/master/nvim/lua/config/keymaps.lua
+local find_file = function()
+    local file_list = {}
+    local list = vim.fn.glob(vim.fn.expand("%:p:h") .. "/*", true, true)
+    local found_cur_file = false
+    local file_idx = 0
+    local count = 0;
+    for k, file_path in pairs(list) do
+        local filename = vim.fs.basename(file_path)
+        local is_file = string.find(filename, '%.')
+        local is_same = filename == vim.fn.expand("%:t")
+        if is_file then
+            table.insert(file_list, filename)
+            count = count + 1
+        end
+        if is_same and is_file then
+            found_cur_file = true
+            file_idx = count
+        end
+    end
+	return {count =count, file_list = file_list, found_cur_file = found_cur_file, file_idx= file_idx}
+end
+local open_next_file = function(opt)
+	local next_file_path
+	local r = find_file()
+    print('Opening next file')
+    if r.found_cur_file then
+		if opt and opt.reverse then
+			if r.file_idx == 1 then
+				next_file_path = r.file_list[r.count]
+			else
+				next_file_path = r.file_list[r.file_idx - 1]
+			end
+		else
+			if r.file_idx == r.count then
+				next_file_path = r.file_list[1]
+			else
+				next_file_path = r.file_list[r.file_idx + 1]
+			end
+		end
+    end
+	if next_file_path then
+		vim.cmd('e ' .. vim.fn.expand("%:p:h") .. '/' .. next_file_path)
+	end
+
+end
+
+-- vim.keymap.set("n", "<A-n>", function ()
+vim.keymap.set("n", "<A-,>", function ()
+    print('opening next file -- reverse')
+	open_next_file({reverse = true})
+end, {})
+-- vim.keymap.set("n", "<A-m>", open_next_file)
+vim.keymap.set("n", "<A-.>", open_next_file)
